9e9ed8fde01e69c47d4c17dae2dde55d54ee5d98
jihyun <ziihyun321@gmail.com>
2022-04-05 16:13:02 +0900

    feat. Add close modal function when click modal outside

--- src/components/modal/AddToCartModal.js
+++ src/components/modal/AddToCartModal.js
@@ -38,6 +38,7 @@ const AddToCartModal = (props) => {
                 <div
                     class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                     <div
+                        onClick={close}
                         class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
                         aria-hidden="true"></div>
                     <span
--- src/components/modal/SearchAddressModal.js
+++ src/components/modal/SearchAddressModal.js
@@ -41,6 +41,7 @@ const SearchAddressModal = (props) => {
                 <div
                     class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                     <div
+                        onClick={close}
                         class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
                         aria-hidden="true"></div>
                     <span
