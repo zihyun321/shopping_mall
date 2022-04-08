1753b9d1e05b7a8e8a5ce6a826b87208562dc371
jihyun <ziihyun321@gmail.com>
2022-04-08 21:02:47 +0900

    feat. Fix redux persist to refresh

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -6,9 +6,8 @@ import storage from "redux-persist/lib/storage";
 const persistConfig = {
     key: "root",
     // localStorage에 저장합니다.
-    storage,
+    storage
     // auth, board, studio 3개의 reducer 중에 auth reducer만 localstorage에 저장합니다.
-    whitelist: ["auth"]
     // blacklist -> 그것만 제외합니다
 };
 
