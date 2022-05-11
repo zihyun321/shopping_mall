f66b0f7301f5816d15eb434e3b08e4152090647d
jihyun <ziihyun321@gmail.com>
2022-05-11 19:24:02 +0900

    feat. Add rollback and commit in server

--- server/routes/order/ctrl.js
+++ server/routes/order/ctrl.js
@@ -7,6 +7,7 @@
  
 
  // DB 연결시작
+ // createPool: 미리 정해진 갯수의 연결을 생성. Request가 발생하면 해당 Req에 연결을 할당하고 다시 반납
  const connection = mysql.createPool({
      host: process.env.DB_HOST, 
      port: process.env.SERVER_PORT, 
@@ -16,6 +17,7 @@
      waitForConnections: true,
      connectionLimit: 10,
      queueLimit: 0,
+     multipleStatements: true
  });
 
 // exports.createOrder = (req, res) => {
@@ -66,17 +68,72 @@ exports.createOrder = (req, res) => {
     console.log('==== createOrder ====');
     console.log('req.body: ', req.body);
     const orderInfo = req.body.orderInfo;
-    const orderItemInfo = req.body.orderItemsInfo;
+    const orderItemsInfo = req.body.orderItemsInfo;
     const productInfo = req.body.productsInfo;
     const userInfo = req.body.userInfo;
 
     console.log('')
 
-    console.log('orderItemInfo: ', orderItemInfo);
-    
-    // try {
+    console.log('orderItemInfo: ', orderItemsInfo);
 
-    // } catch(err) {
-    //     console.log('err: ', err);
-    // }
+    // Order 
+    const insertOrderSql = 'INSERT INTO `order` SET ' + orderInfo;
+    console.log('insertOrderSql: ', insertOrderSql);
+
+    // OrderItem
+    const orderItems = [];
+    orderItemsInfo.forEach(item => {
+        let orderItem = [item.orderId, item.productId, item.orderQuantity, item.customerId, item.orderPrice, item.orderStatus];
+        orderItems.push(orderItem);
+    });
+    const insertOrderItemSql = 'INSERT INTO orderItem (orderId, productId, orderQuantity, customerId, orderPrice, orderStatus) VALUES ' + orderItems;
+    console.log('insertOrderItemSql: ', insertOrderItemSql);
+
+    // Product
+    let updateProductSql = '';
+    productInfo.forEach(function(item) {
+        console.log('item: ', item);
+        // query += mysql.format("UPDATE product SET quantity=? WHERE id=? ;", [item.quantity, item.id]);
+        updateProductSql += `UPDATE product SET quantity=${item.quantity} WHERE id='${item.id}';`;
+    })    
+    console.log('updateProductSql: ', updateProductSql);
+
+    // User
+    let updateUserSql = `UPDATE customer SET points = ${userInfo.points} WHERE id = '${userInfo.id}' `;
+    console.log('updateUserSql: ', updateUserSql);
+
+    // const transaction = new mysql.Transaction();
+    
+    try {
+
+        // 에러남: SyntaxError: await is only valid in async functions and the top level bodies of modules
+        // await new Promise(resolve => transaction.begin(resolve));        
+        // const request = new mysql.Request(transaction);
+
+        // let insertOrderSqlResult = request.query(insertOrderSql);        
+        // let insertOrderItemSqlResult = request.query(insertOrderItemSql);
+        // let updateProductSqlResult = request.query(updateProductSql);
+        // let updateUserSqlResult = request.query(updateUserSql);
+
+        // transaction.commit();
+
+        let insertOrderSqlResult = connection.query(insertOrderSql);        
+        let insertOrderItemSqlResult = connection.query(insertOrderItemSql);
+        let updateProductSqlResult = connection.query(updateProductSql);
+        let updateUserSqlResult = connection.query(updateUserSql);
+
+        connection.commit();
+
+        console.log('insertOrderSqlResult: ', insertOrderSqlResult);
+        console.log('insertOrderItemSqlResult: ', insertOrderItemSqlResult);
+        console.log('updateProductSqlResult: ', updateProductSqlResult);
+        console.log('updateUserSqlResult: ', updateUserSqlResult);
+
+
+    } catch(err) {
+
+        console.log('err: ', err);
+        connection.rollback();
+
+    }
 }
